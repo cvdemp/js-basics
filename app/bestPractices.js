1952bd3a9ae4763b59d0883cd6ede09e985be0dd
Christine Dempsey <cvdemp@gmail.com>
2015-12-01 13:37:52 -0500

    initial commit

---
+++ app/bestPractices.js
@@ -0,0 +1,42 @@
+exports = (typeof window === 'undefined') ? global : window;
+
+/**
+ * This file defines an object with some methods. Some of these methods are
+ * populated incorrectly; your job is to fix them. Other methods are not
+ * populated at all; your job is to fill them out.
+ */
+
+exports.bestPracticesAnswers = {
+  globals : function() {
+    var myObject = {
+      name : 'Jory'
+    };
+
+    return myObject;
+  },
+
+  functions : function(flag) {
+    if (flag) {
+      var getValue = function() { return 'a'; }
+    } else {
+      var getValue = function() { return 'b'; }
+    }
+
+    return getValue();
+  },
+
+  parseInt : function(string) {
+    // var num = parseInt(string)
+    // return num;
+  },
+
+  identity : function(val1, val2) {
+    if (val1 === val2) {
+      var identity = function() { return true; }
+    } else {
+      var identity = function() { return false; }
+    }
+
+    return identity();
+  }
+};
