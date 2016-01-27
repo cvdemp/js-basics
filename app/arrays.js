f4c537bae290c9ded7133bd24065e68acfa26f6b
Christine Dempsey <cvdemp@gmail.com>
2016-01-27 13:55:26 -0500

    array dups

--- app/arrays.js
+++ app/arrays.js
@@ -1,4 +1,4 @@
-exports = (typeof window === 'undefined') ? global : window;
+ exports = (typeof window === 'undefined') ? global : window;
 
 exports.arraysAnswers = {
 
@@ -24,28 +24,45 @@ exports.arraysAnswers = {
   },
 
   prepend : function(arr, item) {
+    arr.unshift(item);
 
+    return arr;
   },
 
   curtail : function(arr) {
+    arr.shift();
 
+    return arr;
   },
 
   concat : function(arr1, arr2) {
-    var result = arr1.concat(arr2)
+    var result = arr1.concat(arr2);
+
+    return result;
   },
 
   insert : function(arr, item, index) {
+    arr.splice(index, 0 , item);
 
+    return arr;
   },
 
   count : function(arr, item) {
-
+    var count = 0;
+    for(var index = 0; index < arr.length; ++index){
+      if(arr[index] == item)
+        count++;
+    }
+    return count;
   },
 
   duplicates : function(arr) {
-
+    var dupArr = arr.filter(function duplicates(elem,pos) {
+      return arr.indexOf(elem) != pos;
+    })
+    return dupArr;
   },
+
 
   square : function(arr) {
 
